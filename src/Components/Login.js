cdf58565926e272a0eed4732b939216531334e52
radams5 <riley.adams66@gmail.com>
2019-04-09 13:43:46 -0600

    front end mockups done

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import axios from 'axios'
 import {connect} from 'react-redux'
 import {updateUser, clearUser} from '../redux/reducer'
+import { threadId } from 'worker_threads';
 
 class Login extends Component {
     constructor(props) {

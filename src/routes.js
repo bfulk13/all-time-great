f93ebb34d68e0f9fbd22b94c4774f953d620d416
radams5 <riley.adams66@gmail.com>
2019-04-09 10:17:24 -0600

    made landing mockup

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Landing from './Components/Landing/Landing'
+import Question from './Components/Question/Question'
+import Vote from './Components/Vote/Vote'
+import Result from './Components/Result/Result'
+import Profile from './Components/Profile/Profile'
+import {Switch, Route} from 'react-router-dom'
+
+
+export default(
+  <Switch>
+    <Route path='/Question' component={Question}/>
+    <Route path='/Vote' component={Vote}/>    
+    <Route path='/Profile' component={Profile}/>
+    <Route path='/Result' component={Result}/>
+    <Route excact path='/' component={Landing}/>
+  </Switch>
+)

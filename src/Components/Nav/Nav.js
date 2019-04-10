15e63ad714bc6b9a3a9d9d375f63633110230e81
Randall Holsinger <randy@Randalls-MacBook-Pro.local>
2019-04-10 13:43:20 -0600

    started nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-import Modal from 'react-responsive-modal';
+// import Modal from 'react-responsive-modal';
+import './Nav.css'
 
 class Nav extends Component {
      constructor(props) {
@@ -20,7 +21,7 @@ class Nav extends Component {
           <nav>
               <img src='https://files.slack.com/files-pri/T039C2PUY-FHHBAQ5EV/goat.png' alt='dabbing-goat'/>
               <ul>
-                  <li><i class="far fa-user"></i></li>
+                  <li><i class="far fa-user user-icon"></i></li>
                   <li><button>Login</button></li>
                   <li></li>
               </ul>

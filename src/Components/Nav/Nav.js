67d4dd2ac832f4b409600868ab7b7fe4096a25e1
Randall Holsinger <randy@Randalls-MacBook-Pro.local>
2019-04-10 17:00:35 -0600

    modal made and styles

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-// import Modal from 'react-responsive-modal';
+import Modal from 'react-responsive-modal';
+import Login from '../Login/Login'
 import './Nav.css'
 
 class Nav extends Component {
@@ -11,21 +12,40 @@ class Nav extends Component {
          }
      }
 
+    
+     openModal = () => {
+         this.setState({
+             open: true
+         })
+     }
 
+     closeModal = () => {
+        this.setState({
+            open: false
+        })
+     }
 
 
 
     render() {
+        const {open} = this.state
         return (
         <div className='nav-wrapper'>
           <nav>
               <img src='https://files.slack.com/files-pri/T039C2PUY-FHHBAQ5EV/goat.png' alt='dabbing-goat'/>
+              <p className='logo'>G.O.A.T.</p>
               <ul>
-                  <li><i class="far fa-user user-icon"></i></li>
-                  <li><button>Login</button></li>
+                  <li><i onClick={this.openModal}className="far fa-user user-icon"></i></li>
+                  <li><i className="fas fa-search search-icon"></i></li>
                   <li></li>
               </ul>
           </nav>
+         <div className='login-modal'>
+          <Modal open={open} onClose={this.closeModal}  center>
+          <Login />
+          </Modal>
+          </div>
+    
            
         </div>
         )

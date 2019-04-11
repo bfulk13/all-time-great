d09d97abb1dc4af620a401797f339d957d935d9f
Toadmeister123 <todd.meister99@gmail.com>
2019-04-11 15:30:53 -0600

    added s3 and front end stuff

--- db/profiles/get_ps_by_likes.sql
+++ db/profiles/get_ps_by_likes.sql
@@ -1,5 +1,5 @@
-select u.username, q.owner_id, sum(likes)
+select u.username, q.owner_id, u.avatar, sum(likes)
 from Questions q 
 join Users u on u.uid = q.owner_id
-group by u.username, q.owner_id
+group by u.username, q.owner_id, u.avatar
 order by sum desc

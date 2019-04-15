1f3bdbbbe13ad001fede89467009e4fa1fae59ce
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-04-15 11:42:18 -0600

    jest testing done

--- src/Logic/LogicBarry.js
+++ src/Logic/LogicBarry.js
@@ -1,7 +1,13 @@
-// export function getTrendingQuestions(){
+import axios from 'axios';
 
-// }
+export function getTrendingQuestions() {
+    let res = axios.get('/api/questions')
+    return res.data
+   
+  }
 
-// export function getPopularProfiles(){
-
-// }
+  export function getPopularProfiles() {
+    let res = axios.get('/api/profiles')
+    return res.data
+  
+  }
--- src/Test.js/LogicBarry.test.js
+++ src/Test.js/LogicBarry.test.js
@@ -1,26 +1,30 @@
-// import {getTrendingQuestions, getPopularProfiles} from '../Logic/LogicBarry';
+import {getTrendingQuestions, getPopularProfiles} from '../Logic/LogicBarry';
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
-// describe('Test getTrendingQuestions function', () => {
-//     it ('should be a function', () => {
-//         expect(typeof getTrendingQuestions).toBe('function')
-//     })
+describe('Test getTrendingQuestions function', () => {
+    it ('should be a function', () => {
+        expect(typeof getTrendingQuestions).toBe('function')
+    })
+
+    it ('should not be an object', () => {
+        expect(typeof getTrendingQuestions).not.toBe('object')
+    })
     
-//     it('should return an array', () => {
-//         expect(typeof WebGLShaderPrecisionFormat).toBe('array')
-//     }
-// })
+    it('should return an array', () => {
+        let mock = new MockAdapter(axios)
+        const data = []
+        mock.onGet('/api/questions').reply(200, data)
+        expect(Array.isArray(data)).toBe(true)
+    })
+})
 
-// describe('Test getPopularProfiles function', () => {
-//     it('should be a function', () => {
-//         expect(typeof getPopularProfiles).toBe('function')
-//     })
+describe('Test getPopularProfiles function', () => {
+    it('should be a function', () => {
+        expect(typeof getPopularProfiles).toBe('function')
+    })
 
-//     it('should return an array', () => {
-//         expect(typeof getPopularProfiles).toBe('array')
-//     })
-
-//     it('should not be null'), () => {
-//         const result = getPopularProfiles()
-//         expect(result).toBeTruthy()
-//     }
-// })
+    it('should not be an object', () => {
+        expect(typeof getPopularProfiles).not.toBe('object')
+    })
+})

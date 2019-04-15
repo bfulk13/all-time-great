4208b8e8494ce731b0c929f968532878f2234175
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-04-15 11:12:41 -0600

    begin jest work

---
+++ src/Logic/LogicBarry.js
@@ -0,0 +1,7 @@
+// export function getTrendingQuestions(){
+
+// }
+
+// export function getPopularProfiles(){
+
+// }
---
+++ src/Test.js/LogicBarry.test.js
@@ -0,0 +1,26 @@
+// import {getTrendingQuestions, getPopularProfiles} from '../Logic/LogicBarry';
+
+// describe('Test getTrendingQuestions function', () => {
+//     it ('should be a function', () => {
+//         expect(typeof getTrendingQuestions).toBe('function')
+//     })
+    
+//     it('should return an array', () => {
+//         expect(typeof WebGLShaderPrecisionFormat).toBe('array')
+//     }
+// })
+
+// describe('Test getPopularProfiles function', () => {
+//     it('should be a function', () => {
+//         expect(typeof getPopularProfiles).toBe('function')
+//     })
+
+//     it('should return an array', () => {
+//         expect(typeof getPopularProfiles).toBe('array')
+//     })
+
+//     it('should not be null'), () => {
+//         const result = getPopularProfiles()
+//         expect(result).toBeTruthy()
+//     }
+// })

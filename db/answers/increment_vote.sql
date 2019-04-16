eedc0aa93a97f80abec14bb7bb7814ac53188945
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-04-16 09:04:25 -0600

    updates

--- db/answers/increment_vote.sql
+++ db/answers/increment_vote.sql
@@ -2,7 +2,7 @@ update Answers
 set vote = vote + 1
 where aid = ${aid};
 
-select count(*)
-from voted
-where Vuser_id= ${uid} and Vquestion_id = ${qid};
+insert into voted (vuser_id, vquestion_id)
+values (${uid}, ${qid})
+
 
--- db/auth/register.sql
+++ db/auth/register.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (email, username, password, avatar)
-VALUES (${email}, ${username}, ${password}, ${avatar})
+VALUES (${email}, ${username}, ${password}, `https://robohash.org${username}`)
 RETURNING uid, username, avatar
